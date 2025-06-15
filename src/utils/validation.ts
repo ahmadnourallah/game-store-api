afd19e75955443a7fdf34e49fe3a2d93dfa4794b
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-06-15 08:23:20 -0400

    Create user validators

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { ClientError } from "../middleware/error.middleware";
-import { validationResult, query } from "express-validator";
+import { validationResult, query, body, param } from "express-validator";
+import { PrismaClient } from "../prisma/src/db/index";
+
+const prisma = new PrismaClient();
 
 const validateResults = (req: Request, res: Response, next: NextFunction) => {
 	const errors = validationResult(req);
@@ -58,4 +61,64 @@ const validateQueries = () => [
 	validateResults,
 ];
 
-export { validateQueries };
+const validateUser = () => [
+	body("name").trim().notEmpty().withMessage("Name cannot be empty"),
+	body("email")
+		.trim()
+		.escape()
+		.notEmpty()
+		.withMessage("Email cannot be empty")
+		.isEmail()
+		.withMessage("Email must be valid")
+		.custom(async (email, { req }) => {
+			const userExists = await prisma.user.findFirst({
+				where: { id: { not: req?.params?.userId }, email },
+			});
+			if (userExists) throw new Error("Email already exists");
+		}),
+	body("password")
+		.trim()
+		.escape()
+		.notEmpty()
+		.withMessage("Password cannot be empty")
+		.isLength({ min: 8, max: 16 })
+		.withMessage("Password must be between 8-16 characters")
+		.matches("[0-9]")
+		.withMessage("Password must contain a number")
+		.matches("(?=.*?[#@$?])")
+		.withMessage("Password must contain a special character"),
+	validateResults,
+];
+
+const validateUserId = () => [
+	param("userId")
+		.trim()
+		.escape()
+		.notEmpty()
+		.withMessage("User's id cannot be empty")
+		.bail()
+		.toInt()
+		.isNumeric()
+		.withMessage("User's id must be a number")
+		.bail(),
+
+	validateResults,
+
+	async (req: Request, res: Response, next: NextFunction) => {
+		const userExists = await prisma.user.findUnique({
+			where: { id: req?.params?.userId as unknown as number },
+		});
+
+		if (!userExists)
+			throw new ClientError({ resource: "Resource not found" }, 404);
+		next();
+	},
+];
+
+const validateLogin = () => [
+	body("email").trim().notEmpty().withMessage("Email cannot be empty"),
+	body("password").trim().notEmpty().withMessage("Password cannot be empty"),
+	validateResults,
+];
+
+export { validateQueries, validateUser, validateUserId, validateLogin };
