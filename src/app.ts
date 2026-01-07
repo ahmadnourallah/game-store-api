2e9a43d844248829be32fb78cce067a35cbb6c58
Ahmad Nourallah <ahmadnurallah@gmail.com>
2026-01-07 13:11:19 -0500

    Allow all origins

--- src/app.ts
+++ src/app.ts
@@ -18,21 +18,7 @@ import "./config/passport.config";
 
 const app = express();
 
-app.use(
-	cors({
-		origin: function (origin: string | undefined, callback: Function) {
-			// allow requests with no origin
-			if (!origin) return callback(null, true);
-			if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
-				var msg =
-					"The CORS policy for this site does not " +
-					"allow access from the specified Origin.";
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(jsonParser);
 
